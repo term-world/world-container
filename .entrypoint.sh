5c14ede826ceaf28a1fc412fae4f47bed15bc879
dluman <dluman@allegheny.edu>
2022-05-25 12:23:15 -0400

    Making all command silent

--- .entrypoint.sh
+++ .entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-set -x
+exec 3>&1 &>/dev/null
 useradd $VS_USER -s /bin/bash
 mkdir /home/$VS_USER
 cd /home
 chown -R $VS_USER:$VS_USER /home/dluman
-cd $VS_USER
+cd /home/$VS_USER
 su $VS_USER
