415b8a809f54a903498a1e6e586a15d858ea1b9d
dluman <dluman@allegheny.edu>
2022-05-25 22:43:22 -0400

    No need to mkdir

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 #exec 3>&1 &>/dev/null
 useradd $VS_USER -s /bin/bash
-mkdir /home/$VS_USER
+#mkdir /home/$VS_USER
 cd /home
 chown -R $VS_USER:$VS_USER /home/$VS_USER
 sudo -i -u dluman code-server
