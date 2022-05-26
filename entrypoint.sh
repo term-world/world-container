c08231fe04be890109d7e95cb0d9c015da598ca1
dluman <dluman@allegheny.edu>
2022-05-26 13:23:11 -0400

    Removing code-server execution at entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 #exec 3>&1 &>/dev/null
 useradd $VS_USER -s /bin/bash
-#mkdir /home/$VS_USER
 cd /home
 chown -R $VS_USER:$VS_USER /home/$VS_USER
-sudo -i -u dluman code-server
+#sudo -i -u $VS_USER code-server
