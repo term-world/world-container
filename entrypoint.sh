55363b416a090bcdaf59a9afab4b8f534babceec
dluman <dluman@allegheny.edu>
2022-06-20 15:05:19 -0400

    Adding ability to assign to user neighborhoods

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-#exec 3>&1 &>/dev/null
+export DISTRICT=`jq ".$VS_USER.district" .registry
+
 useradd $VS_USER -s /bin/bash
+usermod usermod -d /home/$DISTRICT/$VS_USER $VS_USER
+
 cd /home
-chown -R $VS_USER:$VS_USER /home/$VS_USER
+
+chown -R $VS_USER:$VS_USER /home/$DISTRICT/$VS_USER
+
 sudo -i -u $VS_USER code-server
