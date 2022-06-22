064b6825216977cef9ef6e4d753e82a6a7901208
dluman <dluman@allegheny.edu>
2022-06-22 11:22:12 -0400

    Removing jq command from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-export DISTRICT=`jq ".$VS_USER.district" /home/.registry | tr -d '"'`
+#export DISTRICT=`jq ".$VS_USER.district" /home/.registry | tr -d '"'`
 
 useradd $VS_USER -s /bin/bash
 usermod -d /home/$DISTRICT/$VS_USER $VS_USER
 
 cd /home
 
-chown -R $VS_USER:$VS_USER /home/$DISTRICT/$VS_USER
+chown -R $VS_USER:$VS_USER $DISTRICT/$VS_USER
 
-cd /home/$DISTRICT/$VS_USER
+cd $DISTRICT/$VS_USER
 
 sudo -i -u $VS_USER code-server
