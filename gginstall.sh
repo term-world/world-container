ac78f1488627ebe808ab6c930533603ab98d993b
dluman <dluman@allegheny.edu>
2022-06-22 13:02:44 -0400

    Bump version for distribution copy

--- gginstall.sh
+++ gginstall.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/GatorEducator/gatorgrade.git
 cd gatorgrade
-cp -r gatorgrade /usr/local/lib/python3.8/dist-packages/
+cp -r gatorgrade /usr/local/lib/python3.10/dist-packages/
 touch /etc/profile.d/gatorgrader.sh
 echo "alias gatorgrade='python /usr/local/lib/python3.10/dist-packages/gatorgrade/main.py'" >> /etc/profile.d/gatorgrader.sh
 
