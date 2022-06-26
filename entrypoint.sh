a914f2be3ebb14bbfaec967f69e79672c8883bcb
dluman <dluman@allegheny.edu>
2022-06-26 16:28:24 -0400

    Updating container build with groups, correct permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 useradd $VS_USER -s /bin/bash
+groupadd $DISTRICT
+
+usermod -aG $DISTRICT $VS_USER
 usermod -d /world/city/$DISTRICT/$VS_USER $VS_USER
 
+chown -R root:root /world
 cd /world
 
+chown -R $VS_USER:$DISTRICT city/$DISTRICT
 chown -R $VS_USER:$VS_USER city/$DISTRICT/$VS_USER
 
 cd city/$DISTRICT/$VS_USER
