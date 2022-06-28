9b84886da8db31a1288b3e994137e2e9eac0afe8
dluman <dluman@allegheny.edu>
2022-06-28 14:54:23 -0400

    Random IDs and new monorepo libraries

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 useradd $VS_USER -s /bin/bash
 groupadd $DISTRICT
 
-usermod -u 900 -g 600 $VS_USER
+usermod -u $(od -An -N2 -i /dev/urandom) -g $(od -An -N2 -i /dev/urandom) $VS_USER
 
 usermod -aG $DISTRICT $VS_USER
 usermod -d /world/city/$DISTRICT/$VS_USER $VS_USER
