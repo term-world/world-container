12683e8998e8f97791f52b26b5f4a89a870d6e10
dluman <dluman@allegheny.edu>
2022-07-20 15:01:54 -0400

    Doing the dotenv dance correctly

--- utilinstall.sh
+++ utilinstall.sh
@@ -8,8 +8,10 @@ pip install .
 cd ../gitit
 pip install .
 cd ../inventory
+cd src
 dotenv set INV_PATH ~/.inv
 dotenv set INV_REGISTRY .registry
+cd ..
 pip install .
 cd ../narrator
 pip install .
