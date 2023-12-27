783de47ada6bcb19b80b1160f5cb9508cf347829
dluman <dluman@allegheny.edu>
2023-12-27 10:21:52 -0500

    Updating with new inventory references

--- scripts/world-cmd.sh
+++ scripts/world-cmd.sh
@@ -8,7 +8,7 @@ alias use='f(){ python -c "import inventory
 import sys
 inventory.items.use(\"$1\")" "$@"; unset -f f;}; f'
 alias inventory='f(){ python -c "import inventory
-inventory.list.display()"; unset -f f;}; f'
+inventory.registry.display()"; unset -f f;}; f'
 alias remove='f(){ python -c "import inventory
 import sys
 inventory.items.trash(\"$1\",\"$2\")" "$@"; unset -f f;}; f'
