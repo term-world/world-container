67607d85889b39056fdb81db14823fb97a9dc9d3
dluman <dluman@allegheny.edu>
2024-05-20 15:26:52 -0400

    Adding everything, including an _early adventure_

--- scripts/gginstall.sh
+++ scripts/gginstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo python -m pip install gatorgrade
+sudo python -m pip install gatorgrade --break-system-packages
 site=$(python -c "import site; print(f'{site.getsitepackages()[0]}/gatorgrade/main.py')")
 sudo sed -i 's/FILE = "gatorgrade.yml"/FILE = ".gatorgrade.yml"/' $site
