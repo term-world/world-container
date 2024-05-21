67d20c80266e65c41534be26b70a89da6b294c08
dluman <dluman@allegheny.edu>
2024-05-21 15:53:21 -0400

    Setting up container to build locally

--- dockerfiles/topia/api-container/entrypoint.sh
+++ dockerfiles/topia/api-container/entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-echo "export WORLD_NAME=topia"
-
-ln -s /world/.python/bin/python3 /usr/bin/python
-
-sudo code --install-extension /term-world-theme.vsix
-sudo code --install-extension /term-world-launcher.vsix
-sudo code --install-extension /bierner.markdown-checkbox-0.4.0.vsix
-sudo code --disable-getting-started-override
+export WORLD_NAME=topia
+/bin/bash
