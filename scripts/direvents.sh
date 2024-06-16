207a33f17d95b1d72d60b1256852cf98ef115e96
dluman <dluman@allegheny.edu>
2024-06-16 16:40:56 -0400

    Updating containers to report movement to API

--- scripts/direvents.sh
+++ scripts/direvents.sh
@@ -1,4 +1,4 @@
 direvents(){
-    builtin cd "$@" && { [ ! -f .flavor ] || cat .flavor 1>&2; } && { [ ! -f .events ] || . .events; }
+    builtin cd "$@" && { [ ! -f .flavor ] || cat .flavor 1>&2; } && { [ ! -f .events ] || . .events; } && { report; }}
 }
 alias cd=direvents
